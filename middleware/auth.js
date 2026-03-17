0ec41b802b827071267f02f1de56e3319bb1a336
feruz00 <feruzatamyradow@mail.ru>
2026-03-17 08:09:55 +0500

    change cookie key

--- middleware/auth.js
+++ middleware/auth.js
@@ -20,8 +20,8 @@ const protect = catchAsync(async (req, res, next) => {
     req.headers.authorization.startsWith('Bearer ')
   ) {
     token = req.headers.authorization.split(' ')[1];
-  } else if (req.cookies?.jwt) {
-    token = req.cookies.jwt;
+  } else if (req.cookies?.user) {
+    token = req.cookies.user;
   }
 
   // 2️⃣ No token
